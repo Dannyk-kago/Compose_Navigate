eb3aa969544d08da345da1bd6c20b13bec6a3363
Dannyk_kago <kagokimari@gmail.com>
2021-11-27 13:33:48 +0300

    "Initial Commit"

---
+++ app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.composedemo
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.composedemo.ui.theme.ComposeDemoTheme
+
+class MainActivity : ComponentActivity() {
+    private lateinit var navHostController: NavHostController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            navHostController = rememberNavController()
+            SetUpNavGraph(navController = navHostController)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/composedemo/Screen.kt
@@ -0,0 +1,6 @@
+package com.example.composedemo
+
+sealed class Screen(val route:String){
+    object HomeScreen: Screen("home_screen")
+    object ProfileScreen: Screen("profile_screen")
+}
